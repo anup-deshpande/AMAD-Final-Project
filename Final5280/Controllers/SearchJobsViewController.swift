311ab87c391aae9175cdee3991047e95cace1d35
Adwait Tathe <adwait.tathe@gmail.com>
2019-12-07 21:59:52 -0500

    search JobTable View

--- Final5280/Controllers/SearchJobsViewController.swift
+++ Final5280/Controllers/SearchJobsViewController.swift
@@ -7,12 +7,46 @@
 //
 
 import UIKit
+import Firebase
 
 class SearchJobsViewController: UIViewController {
 
+    @IBOutlet weak var searchJobTableView: UITableView!
+    var ref: DatabaseReference!
+    var jobList : [ String] = []
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("Search Jobs")
+        ref = Database.database().reference()
+        searchJobTableView.delegate = self
+        searchJobTableView.dataSource = self
+    }
+    
+    func readJobList() -> [String]
+    {
+        self.jobList = []
+        
+        self.ref.child("jobs").observe(DataEventType.value) { (snapshot) in
+            
+        }
+        
+        return self.jobList
+        
     }
     
 }
+extension SearchJobsViewController : UITableViewDelegate{
+    
+}
+
+extension SearchJobsViewController : UITableViewDataSource{
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 10
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        var cell = tableView.dequeueReusableCell(withIdentifier: "searchJobTableViewCell", for: indexPath) as! SearchJobTableViewCell
+        return cell
+    }
+}
+
+
